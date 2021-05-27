368807a28bf99777dcc9eb9d07ff8fe869b78de6
Vipul <44301794+vipulxd@users.noreply.github.com>
2021-05-27 17:06:23 +0530

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const connect_database = require("./config/database");
 connect_database();
 
@@ -14,7 +15,10 @@ app.use("/api/user", require("./route/Users"));
 const PORT = process.env.PORT || 9000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => console.log(`Server is up and running on PORT ${PORT}`));
